e031aa83bc91f0b7aa0d0d2a4446233a1526c030
Noah <Noah.Marklund0@gmail.com>
2024-07-10 00:12:14 +0200

    BVH p1

--- Ray tracer/Assets/Scripts/Data types/Other/BoundingVolume.cs
+++ Ray tracer/Assets/Scripts/Data types/Other/BoundingVolume.cs
@@ -1,7 +1,25 @@
 using Unity.Mathematics;
-public struct BoundingVolume
+public class BoundingVolume
 {
-    public float3 cornerA;
-    public float3 cornerB;
-    // other node / tri specefic info
+    public float3 min;
+    public float3 max;
+    public int triStart;
+    public int totTris;
+    public int childVolumeAIndex;
+    public int childVolumeBIndex;
+
+    public BoundingVolume(float3 min, float3 max, int triStart = -1, int totTris = -1, int childVolumeAIndex = -1, int childVolumeBIndex = -1)
+    {
+        this.min = min;
+        this.max = max;
+        this.triStart = triStart;
+        this.totTris = totTris;
+        this.childVolumeAIndex = childVolumeAIndex;
+        this.childVolumeBIndex = childVolumeBIndex;
+    }
+
+    public bool IsLeaf()
+    {
+        return childVolumeAIndex == -1 && childVolumeBIndex == -1;
+    }
 };
--- Ray tracer/Assets/Scripts/MeshHelper.cs
+++ Ray tracer/Assets/Scripts/MeshHelper.cs
@@ -1,5 +1,6 @@
 using Unity.Mathematics;
 using UnityEngine;
+using System.Collections.Generic;
 
 // Import utils from Resources.cs
 using Resources;
@@ -8,6 +9,8 @@ using Resources;
 public class MeshHelper : MonoBehaviour
 {
     public Mesh[] Meshes;
+    public int MaxBVHDepth;
+    public int SplitResolution; // ex. 10 -> Each BV split will test 10 increments for each component x,y,z (1000 tests total)
     public Tri2[] LoadOBJ(int meshIndex, float scale)
     {
         Mesh mesh = Meshes[meshIndex];
@@ -35,23 +38,98 @@ public class MeshHelper : MonoBehaviour
         return tri2s;
     }
 
+    private float3 GetTri2Min(params Tri2[] tris)
+    {
+        float3 min = new float3(float.MaxValue, float.MaxValue, float.MaxValue);
+
+        foreach (var tri in tris)
+        {
+            min.x = Mathf.Min(min.x, tri.vA.x, tri.vB.x, tri.vC.x);
+            min.y = Mathf.Min(min.y, tri.vA.y, tri.vB.y, tri.vC.y);
+            min.z = Mathf.Min(min.z, tri.vA.z, tri.vB.z, tri.vC.z);
+        }
+
+        return min;
+    }
+
+    private float3 GetTri2Max(params Tri2[] tris)
+    {
+        float3 max = new float3(float.MinValue, float.MinValue, float.MinValue);
+
+        foreach (var tri in tris)
+        {
+            max.x = Mathf.Max(max.x, tri.vA.x, tri.vB.x, tri.vC.x);
+            max.y = Mathf.Max(max.y, tri.vA.y, tri.vB.y, tri.vC.y);
+            max.z = Mathf.Max(max.z, tri.vA.z, tri.vB.z, tri.vC.z);
+        }
+
+        return max;
+    }
+
+    private float GetBoxArea(float3 vA, float3 vB)
+    {
+        float length = Mathf.Abs(vA.x - vB.x);
+        float width = Mathf.Abs(vA.y - vB.y);
+        float height = Mathf.Abs(vA.z - vB.z);
+
+        float area = 2 * (length * width + width * height + height * length);
+        
+        return area;
+    }
+
+    private void SwapPair<T>(ref T[] array, int indexA, int indexB)
+    {
+        (array[indexB], array[indexA]) = (array[indexA], array[indexB]);
+    }
+
+    // Should be implemented directly in "ConstructBVHFromObj"
+    // void SplitBoundingBox()
+    // {
+    //     return;
+    // }
+
+    private void SplitBoundingVolume(ref List<BoundingVolume> BVs, ref Tri2[] tris, int bvParentIndex, float3 bvParentMin, float3 bvParentMax, int depth)
+    {
+        if (depth < MaxBVHDepth)
+        {
+            // Test dividing the parent BV in a grid formation, incrementing between bvParentMin and bvParentMax
+            float3 diff = bvParentMax - bvParentMin;
+            for (float splitX = bvParentMax.x; splitX < bvParentMax.x; splitX += diff.x/SplitResolution)
+            {
+                for (float splitY = bvParentMax.y; splitY < bvParentMax.y; splitY += diff.y/SplitResolution)
+                {
+                    for (float splitZ = bvParentMax.z; splitZ < bvParentMax.z; splitZ += diff.z/SplitResolution)
+                    {
+                        
+                    }   
+                }
+            }
+        }
+    }
+
+    // Should also return the bounding volumes list in some way, since that data is needed for the BVH
     public (Box[], Tri[]) ConstructBVHFromObj(int meshIndex, float scale)
     {
-        Tri2[] meshTris = LoadOBJ(meshIndex, scale);
+        Tri2[] tris = LoadOBJ(meshIndex, scale);
 
-        // --- Construct BVH ---
+        float3 min = GetTri2Min(tris);
+        float3 max = GetTri2Max(tris);
+        List<BoundingVolume> BVs = new List<BoundingVolume>
+        {
+            // First depth BV
+            new BoundingVolume(min, max, 0, tris.Length, 1, 2)
+        };
+
+        SplitBoundingVolume(ref BVs, ref tris, 0, min, max, 0);
+
+        // --- other ---
 
 
 
-
-
-
-
-
-
+        // --- other ---
 
         Box[] boxes = new Box[2];
         boxes[0] = new Box { vA = 1, vB = 5, materialKey = 0 };
-        return (boxes, Utils.TrisFromTri2s(meshTris));
+        return (boxes, Utils.TrisFromTri2s(tris));
     }
 }
